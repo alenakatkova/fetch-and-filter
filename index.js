d2780d8e343ab7f220f39c42f62dc8ba543681ca
Alena Katkova <katkova.web@gmail.com>
2021-09-28 20:07:07 +0200

    change hash when click on name; show hash on page

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 const searchInput = document.querySelector(".search-input");
 const usersContainer = document.querySelector(".users-container");
-usersContainer.textContent = "CLOCK";
+const user = document.querySelector(".user");
+usersContainer.textContent = "Loading...";
+
 let fetchedUsers;
+
+const onHashChange = () => {
+  user.innerHTML = location.hash;
+};
+
+const showUsersList = (users) => {
+  usersContainer.innerHTML = users.map(user => {
+    return `<li><a href="#${user.name}">${user.name}</a></li>`;
+  }).join("");
+  const links = document.querySelectorAll("a");
+  [...links].forEach(link => {
+    link.addEventListener("click", () => {
+      onHashChange();
+    });
+  })
+};
 
 fetch("https://jsonplaceholder.typicode.com/users")
     .then(response => response.json())
     .then(users => {
       fetchedUsers = users;
-      usersContainer.innerHTML = users.map(user => {
-        return `<li>${user.name}</li>`;
-      }).join("");
+      showUsersList(fetchedUsers);
     })
     .finally(() => {
       searchInput.removeAttribute("disabled");
@@ -21,9 +37,8 @@ searchInput.addEventListener("input", () => {
           .toLowerCase()
           .includes(searchInput.value.toLowerCase()));
 
-  usersContainer.innerHTML = filteredUsers
-      .map(user => {
-          return `<li>${user.name}</li>`;
-      })
-      .join("");
+  showUsersList(filteredUsers);
 });
+
+window.addEventListener("hashchange", onHashChange);
+
