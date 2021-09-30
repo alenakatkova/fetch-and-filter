90106ee66642a4174fd96cec403451d9df905839
Alena Katkova <katkova.web@gmail.com>
2021-09-30 14:40:30 +0200

    show user info on click on on pressing back button

--- index.js
+++ index.js
@@ -2,21 +2,40 @@ const searchInput = document.querySelector(".search-input");
 const usersContainer = document.querySelector(".users-container");
 const user = document.querySelector(".user");
 usersContainer.textContent = "Loading...";
-
 let fetchedUsers;
 
+const generateUserInfo = (userId) => {
+  const chosenUser = [...fetchedUsers].find(user => {
+    return user.id === Number(userId);
+  });
+
+  return `
+      <p>Name: ${chosenUser.name}</p>
+      <p>Username: ${chosenUser.username}</p>
+      <p>Email: ${chosenUser.email}</p>
+      <p>City: ${chosenUser.address.city}</p>
+      <p>Company: ${chosenUser.company.name}</p>
+  `
+};
+
 const onHashChange = () => {
-  user.innerHTML = location.hash;
+  const newHash = location.hash;
+  if (newHash.length === 0) {
+    user.innerHTML = "";
+  } else {
+    user.innerHTML = generateUserInfo(newHash.slice(3));
+  }
 };
 
 const showUsersList = (users) => {
   usersContainer.innerHTML = users.map(user => {
-    return `<li><a href="#${user.name}">${user.name}</a></li>`;
+    return `<li><a data-id="${user.id}" href="#${'id' + user.id}">${user.name}</a></li>`;
   }).join("");
+
   const links = document.querySelectorAll("a");
   [...links].forEach(link => {
-    link.addEventListener("click", () => {
-      onHashChange();
+    link.addEventListener("click", (e) => {
+      user.innerHTML = generateUserInfo(e.target.dataset.id);
     });
   })
 };
